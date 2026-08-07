7e09b7e0956bc28b2d97c49146634465561a6638
rtepess2-coder <rtepess2@gmail.com>
2026-08-07 19:32:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,5 @@ def trigger_email():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
